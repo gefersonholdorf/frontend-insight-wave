8b5a4a9143dcaaef179eed9db16060acabd960fe
Geferson Holdorf <gefersonholdorf88@gmail.com>
2025-07-24 09:13:26 -0300

    feat: IW02 - Projeto configurado

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+export function App() {
+
+  return (
+    <>
+      <h1 className="bg-green-500">Welcome to My App</h1>
+      <Button>Teste</Button>
+    </>
+  )
+}
